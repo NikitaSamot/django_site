a364ee3bbc059fba63bfa1a46d070183174ed340
NikitaSamot <110569446+NikitaSamot@users.noreply.github.com>
2024-05-05 20:05:56 +0500

    приложение main готово, главная страница и о нас

--- main/views.py
+++ main/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'title': 'hadame',
+        'text_h1': 'Мага мебели'
     }
     return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'text_about': 'Страница о нас'
+    }
+    return render(request, 'main/about.html', context)
