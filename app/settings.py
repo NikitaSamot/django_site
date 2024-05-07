00c92537baef91bef959af421ba64ab06cf89fee
NikitaSamot <110569446+NikitaSamot@users.noreply.github.com>
2024-05-07 22:28:06 +0500

    debug_toolbar

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'debug_toolbar',
+
     #custom apps
     'main',
     'goods',
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -125,7 +129,12 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
