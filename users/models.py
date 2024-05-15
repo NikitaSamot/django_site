eadaf99cf02a6fcf398df9b4d30c134f1ced98bc
NikitaSamot <110569446+NikitaSamot@users.noreply.github.com>
2024-05-15 23:39:55 +0500

    готова форма и контроллер  логина пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to='user_images', blank=True, null=True, verbose_name='Изображение')
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('id',)
+
+    def __str__(self) -> str:
+        return self.username
